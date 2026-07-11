f1800b0bae4222181bbb9f935bab392c0a4d47c4
haniffzain <haniffsp@gmail.com>
2026-07-12 00:04:54 +0800

    Initial commit: Struktur asal projek KindOS

---
+++ rebrand_all_logos.sh
@@ -0,0 +1,7 @@
+[/]
+menu-button-icon='/home/developer/Documents/logo.svg'
+menu-button-icon-size=45
+prefs-visible-page=0
+recently-installed-apps=['Waydroid.desktop', 'waydroid.com.google.android.contacts.desktop', 'waydroid.com.android.vending.desktop', 'waydroid.com.google.android.apps.messaging.desktop', 'net.davidotek.pupgui2.desktop', 'steam.desktop', 'gimp.desktop', 'org.mozilla.firefox.desktop', 'org.libreoffice.LibreOffice.base.desktop', 'org.libreoffice.LibreOffice.math.desktop', 'org.libreoffice.LibreOffice.draw.desktop', 'org.libreoffice.LibreOffice.desktop', 'org.libreoffice.LibreOffice.writer.desktop', 'org.libreoffice.LibreOffice.calc.desktop', 'org.libreoffice.LibreOffice.impress.desktop']
+search-entry-border-radius=(true, 25)
+update-notifier-project-version=73
